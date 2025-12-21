dc8f438f52ead3a714e73f33cacfcd68f00c9446
Andrew Palski <ampalski@gmail.com>
2025-12-21 00:05:58 -0700

    dropping JET

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,11 @@
 using OrbitPropagationLibrary
 using OrbitPropagationLibrarySOFA
 using Aqua
-using JET
 using Test
 
 const tests = [
     "Propagations/Numerical",
     "Extras/aqua",
-    "Extras/jet",
 ]
 @testset "OrbitPropagationLibrary.jl" begin
     @testset "Test $t" for t in tests
