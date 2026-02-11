467e84da1173fd3bf73ea34428fd41fad83617d6
Andrew Palski <ampalski@gmail.com>
2026-02-10 21:29:57 -0700

    fixing angle wrapping code

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,19 +1,9 @@
 function wrapto2pi(input::Float64)
-    p2 = 2 * pi
-    while input < 0
-        input += p2
-    end
-
-    return input % p2
+    return rem2pi(input, RoundDown)
 end
 
 function wraptopi(input::Float64)
-    p2 = 2 * pi
-    w = input % (p2)
-    if abs(w) > 0.5
-        w -= sign(input) * p2
-    end
-    return w
+    return rem2pi(input, RoundNearest)
 end
 
 function cross(a::AbstractVector, b::AbstractVector)
